05a181951febddd732c617869f03313a4e5a581d
YogitaDevi <117684801+YogitaDevi@users.noreply.github.com>
2022-12-16 18:11:50 +0530

    [yogita] Find frequency of word in hash table

---
+++ UC1_CreateHashTable/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using UC1_CreateHashTable.Hashtable;
+
+MyMapNode<string, string> hashtable = new MyMapNode<string, string>(5);
+
+hashtable.Add("0", "To");
+hashtable.Add("1", "be");
+hashtable.Add("2", "or");
+hashtable.Add("3", "not");
+hashtable.Add("4", "to");
+hashtable.Add("5", "be");
+
+string getword = hashtable.GetElement("2");
+Console.WriteLine("2th index value is : " + getword);
+//string getword = hashtable.GetElement("5");
+//Console.WriteLine("3th index value is : " + getword);
