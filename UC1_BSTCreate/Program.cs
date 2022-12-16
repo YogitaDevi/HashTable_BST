232302a5a0c7c8118501ab904e35c600e992b166
YogitaDevi <yogita.gry@gmail.com>
2022-12-16 17:12:51 +0530

    [yogita] create a BST

---
+++ UC1_BSTCreate/Program.cs
@@ -0,0 +1,11 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using UC1_BSTCreate.BinarySearch;
+
+
+BinarySearch binarytree = new BinarySearch();
+binarytree.InsertingNode(56);
+binarytree.InsertingNode(30);
+binarytree.InsertingNode(70);
+binarytree.Display();
